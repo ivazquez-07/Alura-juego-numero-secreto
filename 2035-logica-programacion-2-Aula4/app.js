f12e0e47ee7ee0628ae1220b9f37516a1ca1adeb
ivazquez-07 <vazquez.ignacio@gmail.com>
2025-01-31 17:55:47 -0300

    Se actualizan los cambios
    Correcciones que permiten reiniciar el juego sin errores

--- 2035-logica-programacion-2-Aula4/app.js
+++ 2035-logica-programacion-2-Aula4/app.js
@@ -3,28 +3,36 @@ let intentos = 0;
 let listaNumerosSorteados = [];
 let numeroMaximo = 3;
 
+asignarTextoElemento('h1', 'Juego del Número Secreto!');
+document.getElementById('valorUsuario').max = numeroMaximo;
+//condicionesIniciales();
+reiniciarJuego();
+
+
 function asignarTextoElemento(elemento, texto) {
     let elementoHTML = document.querySelector(elemento);
     elementoHTML.innerHTML = texto;
+    return;
 }
 
 function verificarIntento() {
     let numeroDeUsuario = parseInt(document.getElementById('valorUsuario').value);
-    
+
     if (numeroDeUsuario === numeroSecreto) {
-        asignarTextoElemento('p',`Acertaste el número en ${intentos} ${(intentos === 1) ? 'vez' : 'veces'}`);
+        asignarTextoElemento('p', `Acertaste el número en ${intentos} ${(intentos === 1) ? 'vez' : 'veces'}`);
         document.getElementById('reiniciar').removeAttribute('disabled');
-        document.querySelector('#intentar').setAttribute('disabled','true');
+        document.querySelector('#intentar').setAttribute('disabled', 'true');
     } else {
         //El usuario no acertó.
         if (numeroDeUsuario > numeroSecreto) {
-            asignarTextoElemento('p','El número secreto es menor');
+            asignarTextoElemento('p', 'El número secreto es menor');
         } else {
-            asignarTextoElemento('p','El número secreto es mayor');
+            asignarTextoElemento('p', 'El número secreto es mayor');
         }
         intentos++;
         limpiarCaja();
     }
+    return;
 }
 
 function limpiarCaja() {
@@ -32,17 +40,16 @@ function limpiarCaja() {
 }
 
 function generarNumeroSecreto() {
-    let numeroGenerado =  Math.floor(Math.random()*numeroMaximo)+1;
+    let numeroGenerado = Math.floor(Math.random() * numeroMaximo) + 1;
 
     console.log(numeroGenerado);
     console.log(listaNumerosSorteados);
     //Si ya sorteamos todos los números
     if (listaNumerosSorteados.length == numeroMaximo) {
-        asignarTextoElemento('p','Ya se sortearon todos los números posibles');
-        document.getElementById('reiniciar').removeAttribute('disabled');
-        document.querySelector('#intentar').setAttribute('disabled','true');
-        listaNumerosSorteados = [];
-        reiniciarJuego();
+        asignarTextoElemento('p', 'Ya se sortearon todos los números posibles');
+        console.log("Ya se sortearon todos los números posibles");
+        console.log(listaNumerosSorteados);
+        return;
     } else {
         //Si el numero generado está incluido en la lista 
         if (listaNumerosSorteados.includes(numeroGenerado)) {
@@ -56,11 +63,26 @@ function generarNumeroSecreto() {
 }
 
 function condicionesIniciales() {
-    asignarTextoElemento('h1','Juego del número secreto!');
-    asignarTextoElemento('p',`Indica un número del 1 al ${numeroMaximo}`);
-    numeroSecreto = generarNumeroSecreto();
-    intentos = 1;
-    console.log(numeroSecreto);
+    //Si ya sorteamos todos los números
+    if (listaNumerosSorteados.length == numeroMaximo) {
+        asignarTextoElemento('p', 'Ya se sortearon todos los números posibles');
+        console.log("Ya se sortearon todos los números posibles");
+        document.getElementById('reiniciar').removeAttribute('disabled');
+        console.log("Se habilita el reinicio del juego");
+        document.getElementById('intentar').setAttribute('disabled', 'true');
+        console.log("Se deshabilita el botón de intentos");
+        listaNumerosSorteados = [];
+        console.log("Se inicializa el array de lista de números.");
+    } else {
+        asignarTextoElemento('p', `Indica un número del 1 al ${numeroMaximo}`);
+        numeroSecreto = generarNumeroSecreto();
+        intentos = 1;
+        console.log(numeroSecreto);
+        //Deshabilitar el botón de nuevo juego
+        document.querySelector('#reiniciar').setAttribute('disabled', 'true');
+        document.getElementById('intentar').removeAttribute('disabled');
+    }
+    return;
 }
 
 function reiniciarJuego() {
@@ -70,9 +92,6 @@ function reiniciarJuego() {
     //Generar el número aleatorio
     //Inicializar el número intentos
     condicionesIniciales();
-    //Deshabilitar el botón de nuevo juego
-    document.querySelector('#reiniciar').setAttribute('disabled','true');
-    document.getElementById('intentar').removeAttribute('disabled');
-}
 
-condicionesIniciales();
+    return;
+}
